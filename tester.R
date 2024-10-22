2a17ae63be6bb1ab2d0aeebaaa803453c700e3b7
PingC20 <110446588+PingC20@users.noreply.github.com>
2024-10-22 21:36:38 +0100

    Create tester.R

---
+++ tester.R
@@ -0,0 +1,9 @@
+install.packages("shinylive")
+
+# Copy "Hello World" from `{shiny}`
+system.file("examples", "01_hello", package="shiny") |>
+    fs::dir_copy("myapp", overwrite = TRUE)
+
+shinylive::export("myapp", "site")
+
+httpuv::runStaticServer("site/")
